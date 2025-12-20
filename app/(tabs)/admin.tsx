f1278e88a5c61e4381ac9f031f914de2797aa166
Rafail Zorzos <rafail.zorzos@gmail.com>
2025-12-20 17:30:19 +0200

    Decided?

--- app/(tabs)/admin.tsx
+++ app/(tabs)/admin.tsx
@@ -1,10 +1,47 @@
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { useState } from "react";
+import { Button } from "react-native";
 
 export default function Admin() {
+	const [date, setDate] = useState(new Date());
+	const [mode, setMode] = useState<any>('date');
+	const [show, setShow] = useState(false);
+
+	const onChange = (event: any, selectedDate: any) => {
+		const currentDate = selectedDate;
+		setShow(false);
+		setDate(currentDate);
+	};
+
+	const showMode = (currentMode: any) => {
+		setShow(true);
+		setMode(currentMode);
+	};
+
+	const showDatepicker = () => {
+		showMode('date');
+	};
+
+	const showTimepicker = () => {
+		showMode('time');
+	};
+
 	return (
-		<ThemedView style={{ flex: 1 }}>
-			<ThemedText style={{ textAlign: "center" }}>ADMIN SETTINGS</ThemedText>
+		<ThemedView style={{ flex: 1, padding: 20 }}>
+			<ThemedText>{date?.toDateString()}</ThemedText>
+			<Button onPress={showDatepicker} title="Show date picker!" />
+			<Button onPress={showTimepicker} title="Show time picker!" />
+			{show && (
+				<DateTimePicker
+					testID="dateTimePicker"
+					value={date}
+					mode={mode}
+					is24Hour={false}
+					onChange={onChange}
+				/>
+			)}
 		</ThemedView>
 	);
-}
+};
